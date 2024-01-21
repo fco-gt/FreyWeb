57ed85165fce1362f2169f36c9d2af19f73b94dd
Francisco <74993776+fco-gt@users.noreply.github.com>
2024-01-21 14:52:29 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 		</Link>
       </div>
 
-	  <div className="flex md:hidden">
+	  <div className="flex sm:hidden">
 		<Image width={500} alt="Frey Icon" src="https://i.imgur.com/WJeLfdR.png"/>
 	  </div>
     </section>
